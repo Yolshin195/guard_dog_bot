5c2455deba4948d54c40066accbb7b18f26a0100
Yolshin195 <elshin195@gmail.com>
2024-04-14 14:40:01 +0530

    create test version: 0.0.2

--- settings.py
+++ settings.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_USER_ID = int(getenv("BOT_USER_ID"))
-BOT_CHAT_ID = int(getenv("BOT_CHAT_ID"))
+BOT_TOKEN = getenv("GDB_BOT_TOKEN")
+BOT_USER_ID = int(getenv("GDB_BOT_USER_ID"))
+BOT_CHAT_ID = int(getenv("GDB_BOT_CHAT_ID"))
 
-STATIC_DIR = getenv("STATIC_DIR", default="static/")
-VIDEO_RECORDING_TIME = 10
+STATIC_DIR = getenv("GDB_STATIC_DIR", default="static/")
+VIDEO_RECORDING_TIME = int(getenv("GDB_VIDEO_RECORDING_TIME", default="10"))
